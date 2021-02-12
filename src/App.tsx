f591a804a86b211c692f33af4d214efbdb9723d1
Almaz Kaliyev <almaz@kaliyev.dev>
2021-02-12 15:57:57 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default (): React.ReactElement => <h1>Digital Spectr Hot</h1>;
---
+++ webpack.config.ts
@@ -0,0 +1,106 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const config: webpack.Configuration = {
+  mode: isDevelopment ? 'development' : 'production',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[fullhash].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      Object.assign(
+        {},
+        {
+          inject: true,
+          template: path.resolve(__dirname, 'public/index.html'),
+        },
+        isDevelopment ? {
+          minify: {
+            removeComments: true,
+            collapseWhitespace: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true,
+            removeEmptyAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            keepClosingSlash: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+          },
+        } : undefined
+      )
+    ),
+    new ReactRefreshWebpackPlugin(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+    quiet: true,
+    open: true,
+    hot: true
+  },
+  optimization: {
+    minimize: !isDevelopment,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            ecma: 2020,
+          },
+          compress: {
+            ecma: 5,
+            comparisons: false,
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true,
+          },
+        }
+      })
+    ],
+    splitChunks: {
+      chunks: 'all',
+      name: false,
+    }
+  }
+};
+
+export default config;
